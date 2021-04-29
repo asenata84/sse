15a8a53d552df2d9de2ad1582a6ad23075f66cbe
Asen Tolsuzov <asen.tolsuzov@scalefocus.com>
2021-04-29 15:45:03 +0300

    Refactoring

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
 app.use(express.static("public"));
 app.use(cors());
 
 app.get("/events", function (req, res) {
-  req.on("close", () => {
-    clearInterval(interval);
-    res.end();
-  });
-
-  req.on("error", (err) => {
-    console.log("%c =========== err >>", "color:#669851;font-size:12px", err);
-    clearInterval(interval);
-    res.end();
-  });
-
   res.writeHead(200, {
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
@@ -27,11 +17,24 @@ app.get("/events", function (req, res) {
 
   const interval = setInterval(() => {
     res.write(`data: Event ${i}\n\n`);
+    i++;
 
     console.log("%c =========== sending >>", "color:#669851;font-size:12px", i);
-
-    i++;
   }, 2000);
+
+  const removeInterval = () => {
+    clearInterval(interval);
+    res.end();
+  };
+
+  req.on("close", () => {
+    removeInterval();
+  });
+
+  req.on("error", (err) => {
+    console.log("%c =========== err >>", "color:#669851;font-size:12px", err);
+    removeInterval();
+  });
 });
 
 app.listen(3000, () => console.log("SSE app listening on port 3000!"));
